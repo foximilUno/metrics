35703d6ff19a3222428d6274ceb16fa805ede852
Foxalt <bleid29@yandex.ru>
2022-02-21 11:40:24 +0300

    increment 2 - WIP

--- internal/handlers/metric_handler.go
+++ internal/handlers/metric_handler.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	//defaultApplicationType = "text/plain"
-	servicePath = "update"
+	defaultApplicationType = "text/plain"
+	servicePath            = "update"
 )
 
 var allowedTypes = map[string]string{
@@ -29,17 +29,17 @@ func SaveMetrics(s repositories.MetricSaver) http.HandlerFunc {
 		}
 		//TODO убрал проверку header, автотест не проходит хотя в задании написано
 		//check content type only defaultApplicationType
-		//if r.Header.Get("Content-type") != defaultApplicationType {
-		//	w.Header().Add("Allowed", "text/plain")
-		//	http.Error(w, "Allowed text/plain only", http.StatusUnsupportedMediaType)
-		//	return
-		//}
+		if r.Header.Get("Content-type") != defaultApplicationType {
+			w.Header().Add("Allowed", "text/plain")
+			http.Error(w, "Allowed text/plain only", http.StatusUnsupportedMediaType)
+			return
+		}
 
 		//check elements in path
 		segments := strings.Split(strings.TrimLeft(r.URL.Path, "/"), "/")
 
 		if segments[0] != servicePath {
-			http.Error(w, "path must be pattern like /update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>", http.StatusBadRequest)
+			http.Error(w, "path must be pattern like /update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>", http.StatusNotFound)
 			return
 		}
 
@@ -64,7 +64,7 @@ func SaveMetrics(s repositories.MetricSaver) http.HandlerFunc {
 			}
 			s.SaveCounter(segments[2], val)
 		default:
-			http.Error(w, fmt.Sprintf("Bad request: %s cant be, use %s", segments[1], reflect.ValueOf(allowedTypes).MapKeys()), http.StatusBadRequest)
+			http.Error(w, fmt.Sprintf("Bad request: %s cant be, use %s", segments[1], reflect.ValueOf(allowedTypes).MapKeys()), http.StatusNotImplemented)
 			return
 		}
 
