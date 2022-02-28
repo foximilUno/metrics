0cc286dea18ab82d029565bda72a89a23ae12f01
Foxalt <bleid29@yandex.ru>
2022-03-01 02:00:33 +0300

    increment 4 - in tests only textplain methods

--- internal/handlers/metric_handler_test.go
+++ internal/handlers/metric_handler_test.go
@@ -116,7 +116,7 @@ func TestSaveMetrics(t *testing.T) {
 			assert.NoError(t, err)
 			req.Header.Set("Content-type", tt.args.contentType)
 			w := httptest.NewRecorder()
-			h := SaveMetricsViaJSON(storage.NewMapStorage())
+			h := SaveMetricsViaTextPlain(storage.NewMapStorage())
 			h.ServeHTTP(w, req)
 			r := w.Result()
 			body, err := ioutil.ReadAll(r.Body)
