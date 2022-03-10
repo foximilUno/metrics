c79a293b01e09618fb8edf9aa215a8a6146eb348
Foxalt <bleid29@yandex.ru>
2022-03-11 00:34:14 +0300

    increment 6- storeinterval change to duration

--- internal/server/metric_server.go
+++ internal/server/metric_server.go
@@ -3,13 +3,14 @@ package server
 import (
 	"github.com/go-chi/chi"
 	"net/http"
+	"time"
 )
 
 type MetricServerConfig struct {
-	Host          string `json:"host" env:"ADDRESS" envDefault:":8080"`
-	StoreInterval int    `json:"storeInterval" env:"STORE_INTERVAL" envDefault:"300"`
-	StoreFile     string `json:"storeFile" env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool   `json:"isRestored" env:"RESTORE" envDefault:"true"`
+	Host          string        `json:"host" env:"ADDRESS" envDefault:":8080"`
+	StoreInterval time.Duration `json:"storeInterval" env:"STORE_INTERVAL" envDefault:"300s"`
+	StoreFile     string        `json:"storeFile" env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+	Restore       bool          `json:"isRestored" env:"RESTORE" envDefault:"true"`
 }
 
 type server struct {
