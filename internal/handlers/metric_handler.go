d192866583f4a662680acaa867248515d5cde58d
Foxalt <bleid29@yandex.ru>
2022-03-14 00:22:41 +0300

    increment 8 - try compress & decompress

--- internal/handlers/metric_handler.go
+++ internal/handlers/metric_handler.go
@@ -106,7 +106,7 @@ func ReturnData(w http.ResponseWriter, r *http.Request, data []byte) error {
 		encodeToString := base64.RawStdEncoding.EncodeToString(b.Bytes())
 		fmt.Println("compress", encodeToString)
 		w.Header().Set("Content-Type", "application/base64")
-		w.Header().Set("Content-Encoding", "gzip")
+		//w.Header().Set("Content-Encoding", "gzip")
 		_, err = w.Write([]byte(encodeToString))
 	} else {
 		w.Header().Set("Content-Type", "application/json")
